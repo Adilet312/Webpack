5a03e6a838a5011be8e78cf311ca3a2bcdb4ed8b
Adilet312 <adiletm@hotmail.com>
2021-01-09 23:11:32 -0800

    Add input validation.

--- src/main.js
+++ src/main.js
@@ -1,48 +1,35 @@
 import './styles.css';
 
+const user = {
+  firstName:'',
+  lastName:'',
+  email:'',
+  password:'',
+  passwordConfirmation: ''
+}
+let firstName = document.querySelector('.firstName');
+let lastName = document.querySelector('.lastName');
+let email= document.querySelector('.email')
+let password = document.querySelector('.password');
+let passwordConfirmation = document.querySelector('.passwordConfirmation');
+const submit =  document.querySelector('.submit')
 
+submit.addEventListener('click', ()=>{
+  submitForm()
+  console.log(user);
+})
 
-
-
-
-
-const fetchData = async (title) => {
-  let url = `http://www.omdbapi.com/?s=${title}&page=1&apikey=32256b23`;
-  const response = await fetch(url);
-  if(!response.ok){
-    const message = `Error has occured: ${response.status}`
-    throw new Error(message)
+function submitForm(){
+  user.firstName = isValid(firstName.value)  && firstName.value;
+  user.lastName = isValid(lastName.value) && lastName.value;
+  user.email = isValid(email.value) && email.value;
+  user.password = isValid(password.value) && password.value;
+  user.passwordConfirmation = isValid(passwordConfirmation.value) && passwordConfirmation.value;
+}
+function isValid(input){
+  console.log(input);
+  if(input){
+    return true;
   }
-  return await response.json();
+  return false;
 }
-
-const displayData = (data) => {
-  console.log(data);
-  const ul = document.querySelector('.output');
-  const display_data = data.map( movie =>`
-    <li class = 'card-container'>
-    <div class = 'card'>
-      <div class = 'front'>
-        <img src = ${movie.Poster}/>
-      </div>
-      <div class ='back'>
-        <h2>${movie.Title}</h2>
-        <h3>${movie.Year}</h3>
-      </div>
-    </div>
-    </li>
-    `).join('')
-    ul.innerHTML = display_data;
-}
-
-const debounce = (callBack, delay) => {
-  let timeController;
-  return function(...args){
-    timeController && clearTimeout(timeController);
-    timeController = setTimeout( ()=> callBack(...args),delay)
-
-  }
-}
-
-const input = document.querySelector('.search');
-input.addEventListener('input', debounce((e)=> fetchData(e.target.value).then(data => displayData(data.Search)).fetch(error => console.log(error)),3000));
